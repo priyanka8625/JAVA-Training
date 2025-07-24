9e03600bd3d930b8b8844c7b5094e55d17ddbbca
Priyanka Sundalam <ksundalam@gmail.com>
2025-07-24 12:28:12 +0530

    Sliding window

--- Day10_Revision/Sliding window/MaxSubarray.java
+++ Day10_Revision/Sliding window/MaxSubarray.java
@@ -4,22 +4,27 @@ import java.util.Scanner;
 
 public class MaxSubarray {
     static void findSubArraySum(int[] arr, int target){
-        int n = arr.length, left=0, right=0, sum=0, length=0, maxSum=0;
+        int n = arr.length, left=0, right=0, sum=0, start=-1, end=-1, maxSum=0, length=0;
 
         while(right<n){
             sum += arr[right];
 
-            while(left<right && sum >= target){
+            while(left<=right && sum >= target){
                 sum -= arr[left++];
             }
-            if(sum > maxSum){
-                maxSum = Math.max(sum, maxSum);
-                length = right-left+1;
+            if(sum < target && (sum >= maxSum && (right-left+1) > (end-start+1))){
+                maxSum = sum;
+                start = left;
+                end = right;
             }
             right++;
         }
         System.out.println("Max Sum < target : "+maxSum);
-        System.out.println("Length of subarray: "+length);
+        System.out.println("Length of subarray: "+(end-start+1));
+        System.out.println("subarray:");
+        for(int i=start; i<=end; i++){
+            System.out.print(arr[i]+" ");
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
